8bee749b430315dc86de1781c9825fde14b8c7e3
Anna Kolliakou <akolliakou@gmail.com>
2021-03-27 14:57:06 +0000

    First commit

---
+++ personality.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+get "/" do
+  erb :home
+end
+
+get "/page1" do
+  erb :quiz
+end
