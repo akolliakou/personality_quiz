a22be12854e5b72aae156cecd01b60ae6ccab83f
Anna Kolliakou <akolliakou@gmail.com>
2021-03-27 16:03:45 +0000

    Home and first two pages added

--- personality.rb
+++ personality.rb
@@ -8,10 +8,31 @@ configure do
   set :session_secret, 'secret'
 end
 
+def valid_choice?(choice)
+  choice == "a" || choice == "A" || choice == "b" || choice == "B"
+end
+
 get "/" do
   erb :home
 end
 
 get "/page1" do
-  erb :quiz
+  erb :page1
+end
+
+get "/page2" do
+  erb :page2
+end
+
+post "/recharge" do
+  choice = params[:recharge]
+
+  if valid_choice?(choice)
+    session[:recharge] = choice
+    redirect "/page2"
+  else
+    session[:message] = "You can only enter A or B"
+    status 422
+    erb :page1
+  end
 end
